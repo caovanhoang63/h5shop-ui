bfb0f9afa086782a80fd8ccbf630030d2399340a
Bui Thai Hoang <buithaihoang04gl@gmail.com>
2024-12-08 17:00:56 +0700

    Add logic search

--- src/pages/product/CardCategoryFilter.tsx
+++ src/pages/product/CardCategoryFilter.tsx
@@ -5,8 +5,15 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion.tsx";
-import { useState } from "react";
-import { ChevronDownIcon, ChevronUpIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import {
+  ChevronDownIcon,
+  ChevronUpIcon,
+  CirclePlus,
+  Search,
+} from "lucide-react";
+import { Input } from "@/components/ui/input.tsx";
+import { ScrollArea } from "@/components/ui/scroll-area.tsx";
 
 export interface CategoryFilter {
   id: number;
@@ -132,6 +139,7 @@ export const CardCategoryFilter = () => {
   const [categoryState, setCategoryState] =
     useState<CategoryFilter[]>(categories);
   const [idCategorySelected, setIdCategorySelected] = useState<number>();
+  const [searchText, setSearchText] = useState<string>("");
 
   const handleClickCategory = (id: number) => {
     setCategoryState((prevState) => {
@@ -145,31 +153,112 @@ export const CardCategoryFilter = () => {
     setIdCategorySelected(id);
   };
 
+  const normalizeText = (text: string): string => {
+    return text
+      .normalize("NFD") // Tách ký tự và dấu thành dạng riêng biệt.
+      .replace(/[\u0300-\u036f]/g, "") // Loại bỏ dấu (chỉ giữ lại ký tự gốc).
+      .replace(/đ/g, "d") // Thay thế 'đ' thành 'd'.
+      .replace(/Đ/g, "D") // Thay thế 'Đ' thành 'D' (nếu có).
+      .toLowerCase(); // Chuyển tất cả thành chữ thường.
+  };
+
+  const filterCategories = (
+    categories: CategoryFilter[],
+    searchText: string,
+  ): CategoryFilter[] => {
+    if (!searchText) return categories;
+
+    const normalizedSearchText = normalizeText(searchText);
+
+    return categories
+      .map((category) => {
+        const normalizedCategoryName = normalizeText(category.name);
+        console.log(normalizedCategoryName);
+        const filteredChildren = filterCategories(
+          category.children,
+          searchText,
+        );
+
+        const isMatch = normalizedCategoryName.includes(normalizedSearchText);
+
+        if (isMatch || filteredChildren.length > 0) {
+          return { ...category, children: filteredChildren };
+        }
+
+        return null;
+      })
+      .filter((category) => category !== null) as CategoryFilter[];
+  };
+
+  const filteredCategories = filterCategories(categoryState, searchText);
+
   return (
     <Card>
       <CardContent>
         <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
             <AccordionTrigger className={"hover:no-underline"}>
-              Nhóm hàng
-            </AccordionTrigger>
-            <AccordionContent className={"space-y-2"}>
-              {/*Dịch qua trái 12px*/}
               <div
-                style={{
-                  transform: "translateX(-20px)",
-                  width: "250px",
+                className={"flex flex-row justify-between"}
+                style={{ width: "100%" }}
+              >
+                <label>Nhóm hàng</label>
+                <button
+                  className="flex flex-row items-center justify-center hover:bg-gray-200"
+                  style={{
+                    width: "20px",
+                    height: "20px",
+                    borderRadius: "10px",
+                    marginRight: "6px",
+                  }}
+                >
+                  <CirclePlus className="w-5 h-5" />
+                </button>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent className={"space-y-2 p-1"}>
+              <div className="relative flex items-center">
+                <Search className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 transform" />
+                <Input
+                  className={"pl-9"}
+                  placeholder={"Theo mã, tên hàng"}
+                  value={searchText}
+                  onChange={(e) => setSearchText(e.target.value)}
+                />
+              </div>
+              <div
+                className="group flex items-center justify-start hover:bg-gray-100 hover:cursor-pointer"
+                style={{ height: "30px" }}
+                onClick={() => {
+                  setIdCategorySelected(0);
                 }}
               >
-                {categoryState.map((category) => (
+                {idCategorySelected === 0 ? (
+                  <label className={"font-bold text-green-500 ml-6"}>
+                    Tất cả
+                  </label>
+                ) : (
+                  <label className={"ml-6"}>Tất cả</label>
+                )}
+              </div>
+              {/*Dịch qua trái 12px*/}
+              <ScrollArea
+                style={{
+                  transform: "translateX(-20px)",
+                  height: "200px",
+                  width: "calc(100% + 20px)",
+                }}
+              >
+                {filteredCategories.map((category) => (
                   <CardCategoryFilterItem
                     key={category.id}
                     category={category}
                     idSelected={idCategorySelected}
                     onClick={handleClickCategory}
+                    searchText={searchText}
                   />
                 ))}
-              </div>
+              </ScrollArea>
             </AccordionContent>
           </AccordionItem>
         </Accordion>
@@ -183,10 +272,12 @@ export const CardCategoryFilterItem = ({
   category,
   idSelected,
   onClick,
+  searchText,
 }: {
   category: CategoryFilter;
   idSelected?: number;
   onClick?: (id: number) => void;
+  searchText: string;
 }) => {
   // State để quản lý việc đóng/mở item con
   const [isOpen, setIsOpen] = useState(false);
@@ -195,19 +286,34 @@ export const CardCategoryFilterItem = ({
     setIsOpen((prevState) => !prevState);
   };
 
+  useEffect(() => {
+    if (searchText.length > 0) {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+    }
+  }, [searchText]);
+
   return (
     <div className="space-y-2 pl-4">
       <div
         className="relative group flex items-center justify-start hover:bg-gray-100 hover:cursor-pointer"
-        style={{ height: "30px", padding: "0px 5px" }}
+        style={{
+          height: "30px",
+          padding: "0px 5px",
+          backgroundColor: idSelected === category.id ? "#f0f0f0" : "",
+        }}
         onClick={() => onClick && onClick(category.id)}
       >
         {/* Button toggle ở phía trước */}
         {category.children && category.children.length > 0 ? (
           <button
-            onClick={toggleOpen}
             className="mr-2 text-gray-500 hover:text-gray-700"
             style={{ width: "12px", height: "16px" }}
+            onClick={(e) => {
+              e.stopPropagation();
+              toggleOpen();
+            }}
           >
             {isOpen ? (
               <ChevronUpIcon className="w-4 h-4" />
@@ -249,6 +355,7 @@ export const CardCategoryFilterItem = ({
             category={child}
             idSelected={idSelected}
             onClick={onClick}
+            searchText={searchText}
           />
         ))}
       </div>
