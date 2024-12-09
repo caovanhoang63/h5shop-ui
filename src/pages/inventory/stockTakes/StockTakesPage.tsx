42f04177fd6469368b22b1d5a9263e51d6bd3b88
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2024-12-09 09:39:42 +0700

    upd StockTakesPage

--- src/pages/inventory/stockTakes/StockTakesPage.tsx
+++ src/pages/inventory/stockTakes/StockTakesPage.tsx
@@ -85,16 +85,16 @@ export default function InventoryCheckPage() {
             <ArrowLeft className="h-6 w-6" />
           </Link>
           <h1 className="text-xl font-semibold">Kiểm kho</h1>
-        </div>
-        <div className="flex items-center gap-2">
           <div className="flex items-center gap-2 bg-white rounded-lg px-3 py-2 flex-1">
             <Input
               type="search"
-              placeholder="PK000014"
+              placeholder="Thêm sản phẩm"
               className="border-none shadow-none"
             />
             <Plus className="h-5 w-5 text-gray-500" />
           </div>
+        </div>
+        <div className="flex items-center gap-2">
           <Button variant="outline" size="icon">
             <Printer className="h-4 w-4" />
           </Button>
@@ -112,16 +112,16 @@ export default function InventoryCheckPage() {
         <div className="flex-1 h-full">
           <Table className={"h-full"}>
             <TableHeader>
-              <TableRow>
+              <TableRow className={"gap-3"}>
                 <TableHead className="w-[50px]"></TableHead>
                 <TableHead className="w-[50px]">STT</TableHead>
                 <TableHead>Mã hàng</TableHead>
                 <TableHead>Tên hàng</TableHead>
                 <TableHead>DVT</TableHead>
-                <TableHead className="text-right">Tồn kho</TableHead>
-                <TableHead className="text-right">Thực tế</TableHead>
-                <TableHead className="text-right">SL lệch</TableHead>
-                <TableHead className="text-right">Giá trị lệch</TableHead>
+                <TableHead className="text-center">Tồn kho</TableHead>
+                <TableHead className="text-center">Thực tế</TableHead>
+                <TableHead className="text-center">SL lệch</TableHead>
+                <TableHead className="text-center">Giá trị lệch</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
@@ -134,16 +134,21 @@ export default function InventoryCheckPage() {
                   <TableCell className="text-blue-600">{item.code}</TableCell>
                   <TableCell>{item.name}</TableCell>
                   <TableCell>{item.unit}</TableCell>
-                  <TableCell className="text-right">
+                  <TableCell className="text-center">
                     {item.stockQuantity}
                   </TableCell>
-                  <TableCell className="text-right">
-                    {item.actualQuantity}
+                  <TableCell className="text-center items-center flex justify-center">
+                    <Input
+                      type="text"
+                      placeholder="Thêm sản phẩm"
+                      className=" shadow-none w-[40px] text-center"
+                      value={item.actualQuantity}
+                    ></Input>
                   </TableCell>
-                  <TableCell className="text-right text-red-500">
+                  <TableCell className="text-center text-red-500">
                     {item.variance}
                   </TableCell>
-                  <TableCell className="text-right">
+                  <TableCell className="text-center">
                     {item.varianceValue}
                   </TableCell>
                 </TableRow>
