72da688f77093995aa2410d392d9fee405a721b8
nhdhieuu <22520439@gm.uit.edu.vn>
2024-12-28 00:54:50 +0700

    Add: Toastify dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import { MainRoute } from "@/routes/MainRoute.tsx";
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <main>
       <MainRoute />
+      <ToastContainer />
     </main>
   );
 }
