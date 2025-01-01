9bd5b81ab68cdac72d28a8d92139aee24db46e6e
nhdhieuu <22520439@gm.uit.edu.vn>
2025-01-02 01:15:15 +0700

    Add: employee api function implementation

--- src/pages/employee/api/employeeApi.ts
+++ src/pages/employee/api/employeeApi.ts
@@ -1,4 +1,9 @@
-import { Employee, EmployeeCreate } from "@/types/employee/employee.ts";
+import {
+  Employee,
+  EmployeeCreate,
+  EmployeeFilter,
+  EmployeeUpdate,
+} from "@/types/employee/employee.ts";
 import axiosInstance from "@/axiosSetup.ts";
 
 export interface EmployeeResponse {
@@ -7,10 +12,20 @@ export interface EmployeeResponse {
 export interface EmployeeCreateResponse {
   data: boolean;
 }
+export interface EmployeeUpdateResponse {
+  data: boolean;
+}
 
-export async function getEmployee(): Promise<EmployeeResponse> {
+export async function getEmployee(
+  filters: EmployeeFilter,
+): Promise<EmployeeResponse> {
+  const params: EmployeeFilter = {};
+  if (filters.lk_first_name) params.lk_first_name = filters.lk_first_name;
+  if (filters.status) params.status = filters.status;
   try {
-    const response = await axiosInstance.get<EmployeeResponse>("/v1/employee");
+    const response = await axiosInstance.get<EmployeeResponse>("/v1/employee", {
+      params: params,
+    });
     console.log("Fetch success:", response.data);
     return response.data;
   } catch (error) {
@@ -24,6 +39,36 @@ export async function createEmployee(
   try {
     const response = await axiosInstance.post<EmployeeCreateResponse>(
       "/v1/employee",
+      body,
+    );
+    console.log("Fetch success:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+}
+export async function deleteEmployee(
+  id: number,
+): Promise<EmployeeCreateResponse> {
+  try {
+    const response = await axiosInstance.delete<EmployeeCreateResponse>(
+      `/v1/employee/${id}`,
+    );
+    console.log("Fetch success:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+}
+export async function updateEmployee(
+  id: number,
+  body: EmployeeUpdate,
+): Promise<EmployeeUpdateResponse> {
+  try {
+    const response = await axiosInstance.patch<EmployeeCreateResponse>(
+      `/v1/employee/${id}`,
       body,
     );
     console.log("Fetch success:", response.data);
