0a43e17b38c2a7594b299af5b45464810678c30a
nhdhieuu <22520439@gm.uit.edu.vn>
2025-01-02 01:18:10 +0700

    Add: employee crud types

--- src/types/employee/employee.ts
+++ src/types/employee/employee.ts
@@ -20,3 +20,16 @@ export interface EmployeeCreate {
   dateOfBirth: Date;
   gender: "male" | "female" | "other";
 }
+export interface EmployeeUpdate {
+  phoneNumber?: string;
+  address?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  dateOfBirth?: Date | null;
+  gender?: "male" | "female" | "other";
+}
+export interface EmployeeFilter {
+  lk_first_name?: string | null;
+  status?: [] | null;
+}
