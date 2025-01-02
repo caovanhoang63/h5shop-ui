29463ae7897dc31e05b1933f583b4d0ba0f472a7
nhdhieuu <22520439@gm.uit.edu.vn>
2025-01-02 15:31:17 +0700

    Add: setting types

---
+++ src/types/setting/setting.ts
@@ -0,0 +1,16 @@
+export interface Setting {
+  id: number;
+  name: string;
+  value: number;
+  description: string;
+  status: number;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+}
+export interface SettingPaging {
+  page: number;
+  limit: number;
+}
+export interface SettingUpdate {
+  value: number;
+}
