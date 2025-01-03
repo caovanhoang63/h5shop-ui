3d21b9e66e525a691de3992f5884a65e9665c53f
hoangcao <22520457@gm.uit.edu.vn>
2025-01-03 13:33:49 +0700

    increase long polling frequency

--- src/pages/dashboard/DashBoardPage.tsx
+++ src/pages/dashboard/DashBoardPage.tsx
@@ -132,7 +132,7 @@ export const DashBoardPage = () => {
           gtCreatedAt: newGtCreatedAt,
         };
       });
-    }, 5000);
+    }, 30000);
     return () => clearInterval(intervalId);
   }, [auditLog]);
 
